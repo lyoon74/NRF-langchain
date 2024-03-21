7b4e801f738d3be50ea653bd56e8cd4a6594fcd0
lyoon74 <141988838+lyoon74@users.noreply.github.com>
2024-03-21 15:01:00 +0900

    StreamLit-Gemini.py

--- StreamLit-Gemini.py
+++ StreamLit-Gemini.py
@@ -147,7 +147,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vetorestore,openai_api_key):
     #llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    llm=ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
     
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
